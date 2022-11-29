cb3ffd6ca7e1e08dda5a1d1f46a458e9307df733
AnnaHanane <rawan1710@hotmail.com>
2022-11-29 11:56:19 +0100

    ajout d'un cour sur les varéables
    ceation de fichie de cours
    Ajout de varéables de types nombres entires et chaines de caractéres

--- variables.py
+++ variables.py
@@ -1,3 +1,14 @@
+# l'opération d'affectation = permet d'injecter une valeur dans une varéable
+
+my_number1 = 123
+my_number2 = -42
+
+print(my_number1)
+print(my_number2)
+
+
+
+
 my_text1 = " Ceci est une chaine de caractéres"
 my_text2 = ' Ceci est aussi une chaine de caractéres'
 
