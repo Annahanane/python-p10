16e596a6b1446328ad4eb62c98b2243131cbfc49
AnnaHanane <rawan1710@hotmail.com>
2022-12-12 12:49:45 +0100

    Reponses exercice-02

---
+++ exercices-02/exercice-02-02.py
@@ -0,0 +1,15 @@
+# exo 2.2
+# Stockez la valeur `2` dans une variable nommée `number1`.
+# Convertissez la valeur en float et stockez le résultat dans une variable nommée `number1_float`.
+# Affichez le résultat.
+
+# réponse 2.2
+
+number1 = 2
+print(type(number1))
+print(number1)
+number1 = float(number1)
+print(type(number1))
+print(number1)
+number1_float = 2.0
+print(number1_float)
