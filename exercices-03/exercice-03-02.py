783ee4805efc452292359c1733ff85f2793f8370
AnnaHanane <rawan1710@hotmail.com>
2022-12-16 15:29:44 +0100

    Repense exercices-03

---
+++ exercices-03/exercice-03-02.py
@@ -0,0 +1,17 @@
+# exo 3.2
+# Bob veut distribuer tous ses bonbons et chocolats à ses amis.
+# Il a 15 bonbons, 17 chocolats et 3 amis.
+# Combien de bonbons lui restera-t-il ?
+# Calculez le reste de bonbons puis stockez le résultat dans la variable `candies_rest`.
+# Combien de chocolats lui restera-t-il ?
+# Calculez le reste de chocolats puis stockez le résultat dans la variable `chocolates_rest`.
+# Affichez ces résultats.
+
+
+
+# réponse 3.2
+candies_rest = 9 % 3
+chocolates_rest = 17 % 3
+print(candies_rest)
+print(chocolates_rest)
+
