d8f04a3b83d570dda3cf7b54bf284238dcaabe5b
AnnaHanane <rawan1710@hotmail.com>
2022-12-25 12:15:57 +0100

    Reponses exercices-05

---
+++ exercices-05/exercice-05-1.py
@@ -0,0 +1,12 @@
+# exo 5.1
+# Ajoutez de la documentation à la fonction suivante et vérifiez
+# qu'elle s'affiche correctement en utilisant la fonction help()
+
+# réponse 5.1
+def multiplication(a: float, b: float) -> float:
+    return a * b
+""" cette fonction deffinie la multiplication de deux nombres floats renvoie un nombre float"""
+
+help = (multiplication)
+print(help)
+
---
+++ exercices-05/exercice-05-5.py
@@ -0,0 +1,19 @@
+# exo 5.5
+# Trouvez au moins une façon de compter le nombre de lignes de la chaîne de caractère suivante :
+
+my_text = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+
+# réponse 5.5
+print(len(my_text))
+
+my_list1  = my_text.split()
+print(my_list1)
+print(len(my_list1))
+"""len(my_list1) donne le nombre de mots dans my_text"""
+
+
+
+
