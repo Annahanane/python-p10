dfea8e677544fff8f15bc30726d7f12db15f54ad
AnnaHanane <rawan1710@hotmail.com>
2023-01-29 15:55:57 +0100

    modification de la reponse

--- exercices-03/exercice-03-04.py
+++ exercices-03/exercice-03-04.py
@@ -4,7 +4,9 @@
 # Affectez le résultat à la variable `average` et affichez le résultat.
 
 # réponse 3.4
-# average = 35.0
-average = 1+2+3+5+8+13+21 / 7
 
+# numbers = [1, 2, 3, 5, 8, 13, 21]
+# average = sum(numbers) / len(numbers)
+# average = 7.571428571428571
+average = ([1+2+3+5+8+13+21] / 7)
 print(average) 
